440f31a2b2cb43f405abf9e61ed745d40607915b
Alejandro Estevan <ale.sai.33@gmail.com>
2023-10-07 16:49:48 -0300

    add os controllers restantes

---
+++ src/main/java/com/recentro/recentro/controllers/GrauDeRiscoController.java
@@ -0,0 +1,11 @@
+package com.recentro.recentro.controllers;
+
+import com.recentro.recentro.models.GrauDeRisco;
+import com.recentro.recentro.repository.GrauDeRiscoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class GrauDeRiscoController {
+
+    @Autowired
+    GrauDeRiscoRepository productRepository;
+}
---
+++ src/main/java/com/recentro/recentro/controllers/LoteController.java
@@ -0,0 +1,32 @@
+package com.recentro.recentro.controllers;
+
+import com.recentro.recentro.models.Lote;
+import com.recentro.recentro.repository.LoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+
+@Controller
+public class LoteController {
+
+    @Autowired
+    LoteRepository loteRepository;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Lote>> getAllProducts(){
+        List<Lote> productsList = loteRepository.findAll();
+        if(!productsList.isEmpty()) {
+            for(Lote lote : productsList) {
+                Long id = lote.getIdlote();
+                lote.add(linkTo(methodOn(loteController.class).getOneProduct(id)).withSelfRel());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productsList);
+    }
+
+
+}
