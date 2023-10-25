aac09e114ca289840722f51b6ccd11d4239dbecc
Weslley Pereira <113145637+Lelylp@users.noreply.github.com>
2023-10-25 12:20:37 +0000

    controller

--- src/main/java/com/recentro/recentro/controllers/LicenciamentoController.java
+++ src/main/java/com/recentro/recentro/controllers/LicenciamentoController.java
@@ -1,6 +1,5 @@
 package com.recentro.recentro.controllers;
 
-import com.recentro.recentro.models.Licenciamento;
 import com.recentro.recentro.repository.LicenciamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
--- src/main/java/com/recentro/recentro/controllers/LoteController.java
+++ src/main/java/com/recentro/recentro/controllers/LoteController.java
@@ -3,6 +3,7 @@ package com.recentro.recentro.controllers;
 import com.recentro.recentro.models.Lote;
 import com.recentro.recentro.repository.LoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,10 +21,10 @@ public class LoteController {
     public ResponseEntity<List<Lote>> getAllProducts(){
         List<Lote> productsList = loteRepository.findAll();
         if(!productsList.isEmpty()) {
-            for(Lote lote : productsList) {
-                Long id = lote.getIdlote();
-                lote.add(linkTo(methodOn(loteController.class).getOneProduct(id)).withSelfRel());
-            }
+            /*for(Lote lote : productsList) {
+                Long id = lote.getId();
+                lote.add(linkTo(methodOn(LoteController.class).getOneProduct(id)).withSelfRel());
+            }*/
         }
         return ResponseEntity.status(HttpStatus.OK).body(productsList);
     }
