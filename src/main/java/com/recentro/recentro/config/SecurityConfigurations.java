4b6023e4994f9367dca5d15e32bf83803fc3d1f0
eliseusalomao <eliseusalomao20@gmail.com>
2023-11-06 14:30:47 -0300

    Muda método para requisição de listar imovel

--- src/main/java/com/recentro/recentro/config/SecurityConfigurations.java
+++ src/main/java/com/recentro/recentro/config/SecurityConfigurations.java
@@ -39,7 +39,7 @@ public class SecurityConfigurations {
                 .authorizeRequests(authorize -> authorize
                         .antMatchers(HttpMethod.POST, "/user/register").permitAll()
                         .antMatchers(HttpMethod.POST, "/user/login").permitAll()
-                        .antMatchers(HttpMethod.POST, "/imovel/list").permitAll()
+                        .antMatchers(HttpMethod.GET, "/imovel/list").permitAll()
                         .antMatchers(HttpMethod.POST, "/imovel/register").hasRole("ADMIN")
                         .antMatchers("/swagger-ui.html", "/swagger-ui/**", "/v2/api-docs", "/swagger-resources/**", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
